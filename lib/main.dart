adb1d8fd57f871cb4baecf0c58cc9639c4b5fda1
Fidriyanto Rizkillah <fidriyanto.rizkillah@amartha.com>
2021-07-10 15:00:27 +0700

    change homescreen to splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: navyColor,
         canvasColor: lightNavyColor,
       ),
-      home: HomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
